04f8b5a92bbe73b1052c278235da7edfb307736b
Mustapha Boudouch <mustaphaboudouch@gmail.com>
2024-05-04 15:19:11 +0200

    feat: centralize error handling

--- internal/user/handler.go
+++ internal/user/handler.go
@@ -20,108 +20,150 @@ func NewHandler(service UserService) *Handler {
 }
 
 func (h *Handler) RegisterRoutes(mux *mux.Router) {
-	mux.HandleFunc("/users", h.GetAll).Methods(http.MethodGet)
-	mux.HandleFunc("/users/{id}", h.Get).Methods(http.MethodGet)
-	mux.HandleFunc("/users", h.Create).Methods(http.MethodPost)
-	mux.HandleFunc("/users/{id}", h.Update).Methods(http.MethodPatch)
-	mux.HandleFunc("/users/{id}", h.Delete).Methods(http.MethodDelete)
+	mux.Handle("/users", utils.ErrorHandler(h.GetAll)).Methods(http.MethodGet)
+	mux.Handle("/users/{id}", utils.ErrorHandler(h.Get)).Methods(http.MethodGet)
+	mux.Handle("/users", utils.ErrorHandler(h.Create)).Methods(http.MethodPost)
+	mux.Handle("/users/{id}", utils.ErrorHandler(h.Update)).Methods(http.MethodPatch)
+	mux.Handle("/users/{id}", utils.ErrorHandler(h.Delete)).Methods(http.MethodDelete)
 }
 
-func (h *Handler) GetAll(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) GetAll(w http.ResponseWriter, r *http.Request) error {
 	users, err := h.service.GetAll()
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return utils.HTTPError{
+			Code: http.StatusInternalServerError,
+			Err:  err,
+		}
 	}
 
 	if err := utils.WriteJSON(w, http.StatusOK, users); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return utils.HTTPError{
+			Code: http.StatusInternalServerError,
+			Err:  err,
+		}
 	}
+
+	return nil
 }
 
-func (h *Handler) Get(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) Get(w http.ResponseWriter, r *http.Request) error {
 	vars := mux.Vars(r)
 	id, err := strconv.Atoi(vars["id"])
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return utils.HTTPError{
+			Code: http.StatusInternalServerError,
+			Err:  err,
+		}
 	}
 
 	user, err := h.service.Get(id)
 	if err != nil {
 		if err == sql.ErrNoRows {
-			http.Error(w, err.Error(), http.StatusNotFound)
-			return
+			return utils.HTTPError{
+				Code: http.StatusNotFound,
+				Err:  err,
+			}
 		}
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return utils.HTTPError{
+			Code: http.StatusInternalServerError,
+			Err:  err,
+		}
 	}
 
 	if err := utils.WriteJSON(w, http.StatusOK, user); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return utils.HTTPError{
+			Code: http.StatusInternalServerError,
+			Err:  err,
+		}
 	}
+
+	return nil
 }
 
-func (h *Handler) Create(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) Create(w http.ResponseWriter, r *http.Request) error {
 	var input map[string]interface{}
 	if err := utils.ParseJSON(r, &input); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
+		return utils.HTTPError{
+			Code: http.StatusInternalServerError,
+			Err:  err,
+		}
 	}
 
 	if err := h.service.Create(input); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return utils.HTTPError{
+			Code: http.StatusInternalServerError,
+			Err:  err,
+		}
 	}
 
 	if err := utils.WriteJSON(w, http.StatusCreated, nil); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return utils.HTTPError{
+			Code: http.StatusInternalServerError,
+			Err:  err,
+		}
 	}
+
+	return nil
 }
 
-func (h *Handler) Update(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) Update(w http.ResponseWriter, r *http.Request) error {
 	vars := mux.Vars(r)
 	id, err := strconv.Atoi(vars["id"])
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return utils.HTTPError{
+			Code: http.StatusInternalServerError,
+			Err:  err,
+		}
 	}
 
 	var input map[string]interface{}
 	if err := utils.ParseJSON(r, &input); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
+		return utils.HTTPError{
+			Code: http.StatusInternalServerError,
+			Err:  err,
+		}
 	}
 
 	if err := h.service.Update(id, input); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return utils.HTTPError{
+			Code: http.StatusInternalServerError,
+			Err:  err,
+		}
 	}
 
 	if err := utils.WriteJSON(w, http.StatusAccepted, nil); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return utils.HTTPError{
+			Code: http.StatusInternalServerError,
+			Err:  err,
+		}
 	}
+
+	return nil
 }
 
-func (h *Handler) Delete(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) Delete(w http.ResponseWriter, r *http.Request) error {
 	vars := mux.Vars(r)
 	id, err := strconv.Atoi(vars["id"])
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return utils.HTTPError{
+			Code: http.StatusInternalServerError,
+			Err:  err,
+		}
 	}
 
 	if err := h.service.Delete(id); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return utils.HTTPError{
+			Code: http.StatusInternalServerError,
+			Err:  err,
+		}
 	}
 
 	if err := utils.WriteJSON(w, http.StatusNoContent, nil); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return utils.HTTPError{
+			Code: http.StatusInternalServerError,
+			Err:  err,
+		}
 	}
+
+	return nil
 }
---
+++ internal/utils/error.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"net/http"
+)
+
+type HTTPError struct {
+	Code int
+	Err  error
+}
+
+func (e HTTPError) Error() string {
+	return e.Err.Error()
+}
+
+type ErrorHandler func(w http.ResponseWriter, r *http.Request) error
+
+func (f ErrorHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if err := f(w, r); err != nil {
+		if e, ok := err.(HTTPError); ok {
+			http.Error(w, e.Error(), e.Code)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
